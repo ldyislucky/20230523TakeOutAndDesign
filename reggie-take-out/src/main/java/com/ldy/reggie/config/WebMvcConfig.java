221baf56cc8c9b5342a28d28427d7a1f2caa5ce0
ldyislucky <1278950466@qq.com>
2023-06-30 15:48:38 +0800

    先不放在static文档中了，先按照课程来，后边在放到static中

--- reggie-take-out/src/main/java/com/ldy/reggie/config/WebMvcConfig.java
+++ reggie-take-out/src/main/java/com/ldy/reggie/config/WebMvcConfig.java
@@ -16,7 +16,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     /**
      * 设置静态资源映射
-     * 其实把前端的静态资源放在resources的static之中之后，就能够做到静态资源映射了，
+     * 其实把前端的静态资源放在resources的static之中之后，就能够做到静态资源映射了，但是除了
+     * index页面，其它页面的路径都需要加上static
      * 这个配置类就没啥用了，在这里就是熟悉一下，知道有折磨回事儿
      */
     @Override
@@ -27,7 +28,7 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
          */
 
         log.info("开始静态资源映射");
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
 }
