9e01ae84dff63695c590ce90523008666cb89f65
ldyislucky <1278950466@qq.com>
2023-10-06 09:38:39 +0800

    添加注解注释说明

--- mybatiesplus/src/main/java/com/ldy/mpp/MppApplication.java
+++ mybatiesplus/src/main/java/com/ldy/mpp/MppApplication.java
@@ -1,5 +1,6 @@
 package com.ldy.mpp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,8 +8,11 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * @author : ldy
  * @version : 1.0
+ * 注意： @ComponentScan({"com.ldy.mpp.service", "com.ldy.mpp.mapper"})，@MapperScan("com.ldy.mpp.mapper")
+ *       以上2个注解缺一不可，一个是将类注册为bean，一个是将类与mybaties映射
  */
 @ComponentScan({"com.ldy.mpp.service", "com.ldy.mpp.mapper"})
+@MapperScan("com.ldy.mpp.mapper")
 @SpringBootApplication
 public class MppApplication {
     public static void main(String[] args) {
