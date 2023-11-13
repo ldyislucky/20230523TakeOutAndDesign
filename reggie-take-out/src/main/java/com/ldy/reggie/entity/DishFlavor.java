5a8fc2211d4f4779f1f2816e3c6d521b6948e9b8
ldyislucky <1278950466@qq.com>
2023-11-13 17:19:28 +0800

    拦截器失效的原因是因为实体类上没有加注解

--- reggie-take-out/src/main/java/com/ldy/reggie/entity/DishFlavor.java
+++ reggie-take-out/src/main/java/com/ldy/reggie/entity/DishFlavor.java
@@ -1,9 +1,8 @@
 package com.ldy.reggie.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.time.LocalDateTime;
-import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -49,26 +48,31 @@ public class DishFlavor implements Serializable {
     /**
      * 创建时间
      */
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
     /**
      * 更新时间
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
     /**
      * 创建人
      */
+    @TableField(fill = FieldFill.INSERT)
     private Long createUser;
 
     /**
      * 修改人
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Long updateUser;
 
     /**
      * 是否删除
      */
+
     private Integer isDeleted;
 
 
