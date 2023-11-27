92d43de16c4d94e0aadfa116ad060490bd9d4a3b
ldyislucky <1278950466@qq.com>
2023-11-27 14:38:39 +0800

    按照obj obj 格式配置redistemplate,是可以的

--- reggie-take-out/src/test/java/ceshi.java
+++ reggie-take-out/src/test/java/ceshi.java
@@ -3,7 +3,10 @@ import cn.hutool.json.JSONUtil;
 import com.ldy.reggie.ReggieApplication;
 import com.ldy.reggie.entity.Employee;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 
 import java.util.List;
 
@@ -13,6 +16,8 @@ import java.util.List;
  */
 @SpringBootTest(classes = ReggieApplication.class)
 public class ceshi {
+    @Autowired
+    private RedisTemplate<Object,Object> redisTemplate;
     @Test
     public void t1(){
         Employee employee = new Employee();
@@ -20,5 +25,11 @@ public class ceshi {
         employee.setPassword("123456");
         String jsonStr = JSONUtil.toJsonStr(employee);
         System.out.println(jsonStr);
+    }
+    @Test
+    public void t2(){
+        SetOperations<Object, Object> operations = redisTemplate.opsForSet();
+        operations.add("7","退钱！");
+        System.out.println(operations.members("7"));
     }
 }
