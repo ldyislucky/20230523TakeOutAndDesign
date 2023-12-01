111a57735df8068a587c3cc1c2e6714e2e41cac3
ldyislucky <1278950466@qq.com>
2023-12-01 19:15:51 +0800

    完成套餐缓存功能

--- reggie-take-out/src/main/java/com/ldy/reggie/controller/SetmealController.java
+++ reggie-take-out/src/main/java/com/ldy/reggie/controller/SetmealController.java
@@ -7,6 +7,7 @@ import com.ldy.reggie.dto.SetmealDTO;
 import com.ldy.reggie.entity.Setmeal;
 import com.ldy.reggie.service.ISetmealService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,21 +26,30 @@ public class SetmealController {
 
     private final ISetmealService iSetmealService;
 
+    /**
+     * 分页查询并不是很好缓存
+     * @param page
+     * @param pageSize
+     * @return
+     */
     @GetMapping("/page")
     public R<Page<Setmeal>> getPage(Integer page,Integer pageSize){
         Page<Setmeal> setmealPage = new Page<>(page, pageSize);
         iSetmealService.page(setmealPage);
         return R.success(setmealPage);
     }
+    @Cacheable(value = "setmealCache",key = "'套餐_'+#id")
     @GetMapping("{id}")//这种请求方式@PathVariable("id")必不可少，不然路径匹配不上
     public R<SetmealDTO> getSetmealDTO(@PathVariable("id") long id){
         return iSetmealService.getSetmealDTO(id);
     }
+    @CacheEvict(value = "setmealCache",key = "'套餐_'+#setmealDTO.id")
     @PostMapping
     public R<String> addSetmealDTO(@RequestBody SetmealDTO setmealDTO){
         iSetmealService.addSetmealDTO(setmealDTO);
         return R.success("新增套餐成功！");
     }
+    @CacheEvict(value = "setmealCache",key = "'套餐_'+#setmealDTO.id")
     @PutMapping
     public R<String> updateSetmealDTO(@RequestBody SetmealDTO setmealDTO){
         iSetmealService.updateSetmealDTO(setmealDTO);
