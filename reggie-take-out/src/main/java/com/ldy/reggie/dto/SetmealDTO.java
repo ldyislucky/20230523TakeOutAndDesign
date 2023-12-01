a1cabb32b06f0959fd83fd43f03d4b0d46b225c6
ldyislucky <1278950466@qq.com>
2023-12-01 13:13:09 +0800

    完成新增套餐功能！

--- reggie-take-out/src/main/java/com/ldy/reggie/dto/SetmealDTO.java
+++ reggie-take-out/src/main/java/com/ldy/reggie/dto/SetmealDTO.java
@@ -2,11 +2,12 @@ package com.ldy.reggie.dto;
 
 import com.ldy.reggie.entity.Dish;
 import com.ldy.reggie.entity.Setmeal;
+import com.ldy.reggie.entity.SetmealDish;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class SetmealDTO extends Setmeal {
-    private List<Dish> setmealDishes;
+    private List<SetmealDish> setmealDishes;
 }
--- reggie-take-out/src/main/java/com/ldy/reggie/service/impl/SetmealServiceImpl.java
+++ reggie-take-out/src/main/java/com/ldy/reggie/service/impl/SetmealServiceImpl.java
@@ -1,11 +1,19 @@
 package com.ldy.reggie.service.impl;
 
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.toolkit.Db;
 import com.ldy.reggie.dto.SetmealDTO;
+import com.ldy.reggie.entity.Dish;
 import com.ldy.reggie.entity.Setmeal;
+import com.ldy.reggie.entity.SetmealDish;
 import com.ldy.reggie.mapper.SetmealMapper;
+import com.ldy.reggie.service.IDishService;
 import com.ldy.reggie.service.ISetmealService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * <p>
@@ -18,8 +26,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> implements ISetmealService {
 
+    @Transactional
     @Override
     public void addSetmealDTO(SetmealDTO setmealDTO) {
-
+        this.save(setmealDTO);
+        Setmeal setmeal = lambdaQuery().eq(Setmeal::getName, setmealDTO.getName()).one();
+        Long setmealId = setmeal.getId();
+        List<SetmealDish> setmealDishes = setmealDTO.getSetmealDishes();
+        /**
+         * db是mybatisplus版本3.5之后才有的
+         */
+        for (SetmealDish setmealDish : setmealDishes) {
+            setmealDish.setSetmealId(setmealId.toString());
+        }
+        Db.saveOrUpdateBatch(setmealDishes);
     }
 }
